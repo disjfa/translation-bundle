e694011a1e683514ddf6ac084f79b4887ccbdd8d
disjfa <disjfa@disjfa.nl>
2018-12-06 20:27:52 +0000

    Moved menu listener to menu listener

--- Menu/TranslationAdminMenuListener.php
+++ Menu/TranslationAdminMenuListener.php
@@ -2,7 +2,7 @@
 
 namespace Disjfa\TranslationBundle\Menu;
 
-use App\Menu\ConfigureMenuEvent;
+use Disjfa\MenuBundle\Menu\ConfigureMenuEvent;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -30,7 +30,7 @@ class TranslationAdminMenuListener
         try {
             $menu = $event->getMenu();
             $menu->addChild('disjfa_translation', [
-                'label' => $this->translator->trans('menu.translation', [], 'admin'),
+                'label' => $this->translator->trans('menu.translation', [], 'translation'),
                 'route' => 'admin_translator_index'
             ])->setExtra('icon', 'fa-language');
         } catch (RouteNotFoundException $e) {
